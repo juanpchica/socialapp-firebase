b93f84d9e92165db1783a8d0a54ae629881b358f
juanpchica <juanpchica@hotmail.com>
2021-06-03 14:41:16 -0500

    User register fields validated

--- functions/index.js
+++ functions/index.js
@@ -63,17 +63,41 @@ app.post("/scream", (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
 //SignUp route
 app.post("/signup", (req, res) => {
-  // Creating user before login with req data
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle,
-  };
+  const { email, password, confirmPassword, handle } = req.body;
 
-  // TODO: Validate user data
+  // Creating user before login with req data
+  const newUser = { email, password, confirmPassword, handle };
+
+  // Validate user data
+  const errors = {};
+  if (isEmpty(email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(email)) {
+    errors.email = "Email is not valid";
+  }
+
+  if (isEmpty(password)) errors.password = "Must not be empty";
+  if (password !== confirmPassword)
+    errors.confirmPassword = "Passwords must match";
+  if (isEmpty(handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
+
+  //Create doc with user
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
