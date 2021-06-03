27b8573e114e376af51d68fb5a9f8d4f19cce35f
juanpchica <juanpchica@hotmail.com>
2021-06-03 09:59:06 -0500

    Function to add a new scream in firestore added

--- functions/index.js
+++ functions/index.js
@@ -27,3 +27,23 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     })
     .catch((err) => console.error(err));
 });
+
+exports.createScreams = functions.https.onRequest((req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAdd: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: `Something went wrong` });
+      console.error(err);
+    });
+});
