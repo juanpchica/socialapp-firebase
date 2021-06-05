73b67261f37dbd6685465e82c121e0f70154570c
juanpchica <juanpchica@hotmail.com>
2021-06-05 10:54:31 -0500

    user details template modified in dbschema

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -8,4 +8,40 @@ let db = {
       commentCount: 3,
     },
   ],
+  users: [
+    {
+      userId: "dh23ggj5h32g543j5gf43",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T10:59:52.798Z",
+      imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "Lonodn, UK",
+    },
+  ],
+};
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: "N43KJ5H43KJHREW4J5H3JWMERHB",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2019-03-15T10:59:52.798Z",
+    imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "Lonodn, UK",
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "hh7O5oWfWucVzGbHH2pa",
+    },
+    {
+      userHandle: "user",
+      screamId: "3IOnFoQexRcofs5OhBXO",
+    },
+  ],
 };
